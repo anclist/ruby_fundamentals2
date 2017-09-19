4d1ec3d532bc5a8b08beffbb049e9855977d6de5
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-19 16:01:40 -0400

    backwards greetings

---
+++ second-assigment/exercise_05.rb
@@ -0,0 +1,14 @@
+def greet_backwards(name)
+  name.reverse
+end
+
+
+bob = "Bob"
+shirly = "Shirly"
+sue = "Sue"
+andy = "Andy"
+
+puts "Hello, #{greet_backwards(bob)}! Welcome home."
+puts "Hello, #{greet_backwards(shirly)}! Welcome home."
+puts "Hello, #{greet_backwards(sue)}! Welcome home."
+puts "Hello, #{greet_backwards(andy)}! Welcome home."
