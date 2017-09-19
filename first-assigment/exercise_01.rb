e2fd5720f639d645d34ffa2d6c9a1191b736b7fd
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-19 14:43:28 -0400

    Define the rest of condition

--- first-assigment/exercise_01.rb
+++ first-assigment/exercise_01.rb
@@ -14,9 +14,17 @@ puts  "Do you like Dramas?"
 puts"Do you like Comedies?"
   third_answer = gets.chomp.to_s
 
-if first_answer == "yes"
-  puts "Since you like documetaries, I recommend whatching #{documentary}!"
-elsif second_answer == "yes" && third_answer == "yes"
-  puts "Since you don't like documentaries, but you like dramas and comedies, I recommend whatching #{dramedy}"
 
+
+
+if first_answer == "yes"
+  puts "Since you like documetaries, I recommend whatching: #{documentary}!"
+elsif second_answer == "yes" && third_answer == "yes"
+  puts "Since you don't like documentaries, but you like dramas and comedies, I recommend whatching: #{dramedy}"
+elsif second_answer == "yes" && third_answer == "no"
+  puts "Since you don't like documentaries or comdies, I recommend whatching: #{drama}"
+elsif second_answer == "no" && third_answer == "yes"
+  puts "Since you don't like documentaries or dramas, I recommend whatching: #{comedy}"
+else
+  puts "Since you don't like documentaries, dramas or comedies, maybe you should read: #{book}"
 end
