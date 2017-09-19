fdd45095f5cb36e45d131a71b860add36b765cce
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-19 14:13:36 -0400

    Defining the suggestions and asking the questions

---
+++ first-assigment/exercise_01.rb
@@ -0,0 +1,15 @@
+#Defining the suggestions
+
+documentary = "Food, Inc."
+drama = "Dunkirk"
+comedy = "Meet the Parents"
+dramedy = "Forrest Gump"
+book = "To Kill a Mockingbird by Harper Lee"
+
+#Asking the tree questions
+puts "Do you like Documentaries?"
+  docu_answer = gets.chomp.to_s
+puts  "Do you like Dramas?"
+  drama_answer = gets.chomp.to_s
+puts"Do you like Comedies?"
+  comedy_answer = gest.chomp.to_s
