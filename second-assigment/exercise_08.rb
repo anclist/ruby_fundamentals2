bfdf30118902add6eb5c9f6ba8460d94883ce127
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-20 15:38:24 -0400

    fixing second assigment exercise 8

--- second-assigment/exercise_08.rb
+++ second-assigment/exercise_08.rb
@@ -1,24 +1,29 @@
-def speed(distance, minutes)
+def get_speed(person_number)
+  puts "How far did person #{person_number} run (in metres)?"
+  distance = gets.to_f
+  puts puts "How long (in minutes) did person #{person_number} run take to run #{distance} metres?"
+  minutes = gets.to_f
   distance / (minutes * 60)
 end
 
-puts "How far did person 1 run (in metres)?"
-distance1 = gets.to_f
-puts "How long (in minutes) did person 1 run take to run #{distance1} metres?"
-mins1 = gets.to_f
+def result(person_number, speed)
+  puts "Person #{person_number} was the fastest at #{speed} m/s"
+end
 
+def fastest_player?(player_speed, opponent_a_speed, opponent_b_speed)
+  player_speed > opponent_a_speed && player_speed > opponent_b_speed
+end
 
-puts "How far did person 2 run (in metres)?"
-distance2 = gets.to_f
-puts "How long (in minutes) did person 2 take to run #{distance2} metres?"
-mins2 = gets.to_f
+speed1, speed2, speed3 = get_speed(1), get_speed(2), get_speed(3)
 
-
-puts "How far did person 3 run (in metres)?"
-distance3 = gets.to_f
-puts "How long (in minutes) did person 3 take to run #{distance3} metres?"
-mins3 = gets.to_f
-
-speed1, speed2, speed3 = speed(distance1, mins1), speed(distance2, mins2), speed(distance3, mins3)
-
-puts "The fastest speed is #{[speed1, speed2, speed3].min} m/s"
+if fastest_player?(speed3, speed1, speed2)
+  result(3, speed3)
+elsif fastest_player?(speed2, speed1, speed3)
+  result(2, speed2)
+elsif fastest_player?(speed1, speed2, speed3)
+  result(1, speed1)
+elsif speed1 == speed2 && speed2 == speed3
+  puts "Everyone tied at #{speed1} m/s"
+else
+  puts "Well done everyone!"
+end
